db78d287faff9c2797ca7cf5345a76f54fa68fde
BonyChops <office.kentarou@gmail.com>
2020-11-07 16:56:36 +0900

    Init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # microbit-IoT-tutorial
 Created with CodeSandbox
+[#webiotmakers](https://twitter.com/hashtag/webiotmakers)
--- pc.js
+++ pc.js
@@ -1,23 +1,36 @@
 // Remote Example1 - controller
 
 var channel;
-onload = async function(){
+onload = async function () {
 	// webSocketリレーの初期化
-	var relay = RelayServer("achex", "chirimenSocket" );
-	channel = await relay.subscribe("chirimenMbitSensors");
-	messageDiv.innerText="achex web socketリレーサービスに接続しました";
+	var relay = RelayServer("achex", "chirimenSocket");
+	channel = await relay.subscribe("chirimenLED");
+	messageDiv.innerText = "achex web socketリレーサービスに接続しました";
 	channel.onmessage = getMessage;
+	turnOnOff();
 }
 
-function getMessage(msg){ // メッセージを受信したときに起動する関数
-	var mdata = msg.data;
-	messageDiv.innerText = JSON.stringify(mdata);
-	var acc = mdata.acceleration;
-	var mag = mdata.magneticField;
-	accTd.innerText = acc.x + ", " + acc.y + ", " + acc.z;
-	magTd.innerText = mag.x + ", " + mag.y + ", " + mag.z;
-	temTd.innerText = mdata.temperature;
-	briTd.innerText = mdata.brightness;
-	butTd.innerText = mdata.button;
-	timTd.innerText = mdata.time;
+function getMessage(msg) { // メッセージを受信したときに起動する関数
+	messageDiv.innerText = msg.data;
+}
+
+function OnLED() { // LED ON
+	console.log("on")
+	channel.send("LED ON");
+}
+
+function OffLED() { // LED OFF
+	console.log("off")
+	channel.send("LED OFF");
+}
+
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+const turnOnOff = async() => {
+	for (;;) {
+		OnLED();
+		await sleep(1000);
+		OffLED();
+		await sleep(1000);
+	}
 }
